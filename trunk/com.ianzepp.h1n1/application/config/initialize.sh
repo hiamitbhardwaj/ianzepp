0589ace783e472fe627400f37ce818045683e990
ian.zepp <ian.zepp@2a6a8634-10ed-11de-aa17-9100d24c02a7>
2009-05-02 19:30:14 +0000

    Disabled the default site by default.

--- trunk/com.ianzepp.h1n1/application/config/initialize.sh
+++ trunk/com.ianzepp.h1n1/application/config/initialize.sh
@@ -11,6 +11,7 @@ H1N1_SITE     = com.ianzepp.h1n1
 cp -f "${H1N1_BASE}/${H1N1_SITE}/application/config/site.config" "/etc/apache2/sites-available/${H1N1_SITE}"
 
 # Disable and reenable the site
+a2dissite "default"
 a2dissite "${H1N1_SITE}"
 a2ensite  "${H1N1_SITE}"
 
