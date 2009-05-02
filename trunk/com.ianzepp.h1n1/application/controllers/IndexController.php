3163aee4fac4a1d154ea6b19d4b207727aae7db9
ian.zepp <ian.zepp@2a6a8634-10ed-11de-aa17-9100d24c02a7>
2009-05-02 19:42:13 +0000

    Update.

--- trunk/com.ianzepp.h1n1/application/controllers/IndexController.php
+++ trunk/com.ianzepp.h1n1/application/controllers/IndexController.php
@@ -2,7 +2,7 @@
 
 require_once 'Zend/Controller/Action.php';
 
-class MapController extends Zend_Controller_Action
+class IndexController extends Zend_Controller_Action
 {
 	public function indexAction ()
 	{
