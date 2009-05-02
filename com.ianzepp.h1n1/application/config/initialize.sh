73a250a205b61cae6e9cf5528d2001919867a356
ian.zepp <ian.zepp@2a6a8634-10ed-11de-aa17-9100d24c02a7>
2009-05-02 19:09:36 +0000

    Migrated initialize script to the SVN repo.

---
+++ com.ianzepp.h1n1/application/config/initialize.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#
+# This script initializes the site
+#
+
+H1N1_BASE     = dirname (dirname (dirname (dirname ($0))))
+H1N1_SITE     = com.ianzepp.h1n1
+
+# Copy over the site config
+cp -f "${H1N1_BASE}/${H1N1_SITE}/application/config/site.config" "/etc/apache2/sites-available/${H1N1_SITE}"
+
+# Disable and reenable the site
+a2dissite "${H1N1_SITE}"
+a2ensite  "${H1N1_SITE}"
+
+# Restart apache
+/etc/init.d/apache2 restart
